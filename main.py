5ca590c86d12bcb05ab71d730ef5685b9dceab69
Tran GCD <71880839+tranlong45200@users.noreply.github.com>
2024-05-07 12:08:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
+from textblob import TextBlob
 
+def correct_spelling(text):
+    blob = TextBlob(text)
+    return str(blob.correct())
 
 def text_to_pdf(input_file, output_file, cover_image=None, book_title=None, author_name=None):
     c = canvas.Canvas(output_file, pagesize=letter)
@@ -29,21 +33,22 @@ def text_to_pdf(input_file, output_file, cover_image=None, book_title=None, auth
         line_height = 20  # Adjust this value to increase/decrease line height
         word_spacing = 5  # Adjust this value to increase/decrease space between words
         for line in lines:
-            words = line.strip().split()  # Split line into words
+            corrected_line = correct_spelling(line)  # Correct spelling
+            words = corrected_line.strip().split()  # Split line into words
             x = 50  # Initial x-coordinate for each line
             remaining_words = words[:]
             while remaining_words:
-                line_words = remaining_words[:15]  # Get up to 15 words for each line
+                line_words = remaining_words[:15]  # Get up to 20 words for each line
                 remaining_words = remaining_words[15:]  # Remove the words that have been processed
                 line_text = ' '.join(line_words)
                 c.drawString(x, y, line_text)  # Draw the line
                 y -= line_height
                 if y < 50:
-                    c.drawString(550, 30, f"{line_number}")
+                    c.drawString(550, 30, f"Page {line_number}")
                     c.showPage()
                     line_number += 1
                     y = 750
-        c.drawString(550, 30, f"{line_number}")  # Add page number for the last page
+        c.drawString(550, 30, f"Page {line_number}")  # Add page number for the last page
     c.save()
 
 
@@ -54,3 +59,4 @@ if __name__ == "__main__":
     book_title = "Autobiography Moonlight"  # Change this to the title of your book
     author_name = "Tran_GCD"  # Change this to the author's name
     text_to_pdf(input_file, output_file, cover_image, book_title, author_name)
+
